90733d3679262c6f56690cb49fd7167b6d9703bd
Harun Reşid Ergen <83069560+HarunErgen@users.noreply.github.com>
2023-05-10 20:34:00 +0300

    Update GeocodeService.java
    fixed latLng error

--- practice-app/backend/disasterresponse/src/main/java/com/a1/disasterresponse/service/GeocodeService.java
+++ practice-app/backend/disasterresponse/src/main/java/com/a1/disasterresponse/service/GeocodeService.java
@@ -4,12 +4,10 @@ import com.a1.disasterresponse.model.GeocodeData;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
 import com.google.maps.GeoApiContext;
 import com.google.maps.GeocodingApi;
 import com.google.maps.model.GeocodingResult;
-
-
+import com.google.maps.model.LatLng;
 
 @Service
 public class GeocodeService {
@@ -25,15 +23,18 @@ public class GeocodeService {
     public GeocodeData geocodeAddress(String address) throws Exception {
         GeocodingResult[] results = GeocodingApi.geocode(geoApiContext, address).await();
         if (results.length > 0) {
-            return new GeocodeData(results[0].geometry.location.lat, results[0].geometry.location.lng, address);
+            LatLng coordinates = results[0].geometry.location;
+            return new GeocodeData(coordinates.lat, coordinates.lng, address);
         }
         return null;
     }
 
     public GeocodeData reverseGeocodeCoordinates(double lat, double lng) throws Exception {
-        GeocodingResult[] results = GeocodingApi.reverseGeocode(geoApiContext, lat, lng).await();
+        LatLng latLng = new LatLng(lat, lng);
+        GeocodingResult[] results = GeocodingApi.reverseGeocode(geoApiContext, latLng).await();
         if (results.length > 0) {
-            return new GeocodeData(lat, lng, results[0].formattedAddress);
+            String formattedAddress = results[0].formattedAddress;
+            return new GeocodeData(lat, lng, formattedAddress);
         }
         return null;
     }
