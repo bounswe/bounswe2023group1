b541cb574e1ab299a4f327f1ad8f1d77721f5a53
furknbulbul <furknblbl@gmail.com>
2023-05-12 17:20:26 +0300

    frontend added for post reqeust

--- practice-app/frontend/disasterresponse/src/pages/ImdbApi/ImdbPage.jsx
+++ practice-app/frontend/disasterresponse/src/pages/ImdbApi/ImdbPage.jsx
@@ -3,40 +3,69 @@ import { useState } from 'react'
 import axios from 'axios'
 import './ImdbPage.css'
 
-export default function ImdbPage() {
+const ImdbPage =() =>  {
     const [query , setQuery] = useState("");
     const [movies, setMovies] = useState([]);
 
-    
 
+
+    
     const getMovies = () => {
         axios.get(`api/movie/getMovies?query=${encodeURIComponent(query)}&limit=5`).then(res => {
             setMovies(res.data)
         }).catch(error =>{
             console.log(error)
         })
-    }
-    
-    return (
-        <div className="movieSearch">
-            <h2>Movie Search</h2>
-            <form onSubmit={(e) => {e.preventDefault(); getMovies()}}>
-                <input type="text" placeholder="Enter movie query" onChange={e => setQuery(e.target.value)} />
-            <button type="submit">Search</button>
-            </form>
-            <div className="movieResults">
-            {movies.map(movie => (
-                <div className="movieCard" key={movie.id}>
-                <h3>{movie.title}</h3>
-                <img src={movie.image.url} alt={movie.title} />
-                </div>
-            ))}
-            </div>
-        </div>
-        );
-
-}
+    };
+    const submitRating = (movieTitle, rating) => {
+        axios
+          .post(`api/movie/rateMovie`, {
+            title: movieTitle,
+            rating: rating,
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+        };
         
+    return (
+            <div className="movieSearch">
+              <h2>Movie Search</h2>
+
+              <form onSubmit={(e) => { e.preventDefault(); getMovies() }}>
+                <input
+                  type="text"
+                  placeholder="Enter movie query"
+                  onChange={(e) => setQuery(e.target.value)}
+                />
+                <button type="submit">Search</button>
+              </form>
+
+              <div className="movieResults">
+                {movies.map((movie) => (
+                  <div className="movieCard" key={movie.id}>
+                    <h3>{movie.title}</h3>
+                    <img src={movie.image.url} alt={movie.title} />
+                    <form
+                      onSubmit={(e) => {
+                        e.preventDefault();
+                        submitRating(movie.title, e.target.rating.value);
+                        e.target.reset();
+                      }}
+                    >
+                      <input type="number" name="rating" min="1" max="5" />
+                      <button type="submit">Submit Rating</button>
+                    </form>
+            
+                  </div>
+                ))}
+              </div>
+            </div>
+          );
+    
+        
+};
+export default ImdbPage;
     
 
 
