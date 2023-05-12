0d6e78a5289d27678f1938f416b87bbae105ff80
furknbulbul <furknblbl@gmail.com>
2023-05-12 20:50:02 +0300

    restcontroller added to controller

--- practice-app/backend/disasterresponse/src/main/java/com/a1/disasterresponse/controller/MovieController.java
+++ practice-app/backend/disasterresponse/src/main/java/com/a1/disasterresponse/controller/MovieController.java
@@ -5,14 +5,13 @@ import com.a1.disasterresponse.model.WatchedItem;
 import com.a1.disasterresponse.service.MovieService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.List;
 
 
-@Controller
+@RestController
 @RequestMapping("/movie")
 public class MovieController {
 
