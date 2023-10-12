2922d793c28839ac7af1c03190f784048b4532ca
alitpc25 <alitpc2525@gmail.com>
2023-10-12 23:07:03 +0300

    BE-255 created the Spring Boot project with log configurations, application properties configuration, general exception handling mechanism and Swagger implementation.

---
+++ resq/backend/resq/src/main/java/com/groupa1/resq/config/ResqAppProperties.java
@@ -0,0 +1,18 @@
+package com.groupa1.resq.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("file:${resq.appdir}/resq/conf/appparam.txt")
+@Getter
+public class ResqAppProperties {
+
+    //If a service wants to reach properties, it should first take it with Dependency Injection @Autowired
+    //And then, with the get methods provided by @Getter annotation of lombok, it can reach the properties.
+
+    @Value("${server.port}")
+    private String serverPort;
+}
---
+++ resq/backend/resq/src/main/java/com/groupa1/resq/controller/UserController.java
@@ -0,0 +1,27 @@
+package com.groupa1.resq.controller;
+
+import com.groupa1.resq.config.ResqAppProperties;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private ResqAppProperties resqAppProperties;
+
+    @GetMapping("/getUser")
+    public String index() {
+        log.trace("A TRACE Message");
+        log.debug("A DEBUG Message");
+        log.info("An INFO Message");
+        log.warn("A WARN Message");
+        log.error("An ERROR Message");
+        return resqAppProperties.getServerPort();
+    }
+}
