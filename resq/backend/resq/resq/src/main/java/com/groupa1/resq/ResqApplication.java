b137a0e9a8ad01ccb4750b9e51fb5183045de9dc
alitpc25 <alitpc2525@gmail.com>
2023-10-12 19:33:05 +0300

    Created project dir and Spring Boot app

---
+++ resq/backend/resq/resq/src/main/java/com/groupa1/resq/ResqApplication.java
@@ -0,0 +1,13 @@
+package com.groupa1.resq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResqApplication.class, args);
+	}
+
+}
