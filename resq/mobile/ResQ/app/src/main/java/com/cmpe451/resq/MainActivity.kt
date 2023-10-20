65b6ec85228f921ec745db1e432113fce1f553f4
Harun Reşid Ergen <ergenharunresid@hotmail.com>
2023-10-20 14:53:27 +0300

    Add MVVM design packages

--- resq/mobile/ResQ/app/src/main/java/com/cmpe451/resq/MainActivity.kt
+++ resq/mobile/ResQ/app/src/main/java/com/cmpe451/resq/MainActivity.kt
@@ -18,10 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ResQTheme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Greeting("Android")
                 }
             }
