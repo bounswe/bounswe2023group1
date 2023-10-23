9690b6664f297381cc49a2e8924d21693499f709
kubraaksux <hatice.aksu@boun.edu.tr>
2023-10-24 02:26:33 +0300

    problems fix front

--- resq/frontend/src/SignIn.js
+++ resq/frontend/src/SignIn.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link as RouterLink, useHistory } from 'react-router-dom';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
--- resq/frontend/src/index.js
+++ resq/frontend/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App /> {/* Render the App component here */}
+    <App /> 
   </React.StrictMode>,
   document.getElementById('root')
 );
