da7d0c51c2e36aa4d6ba04a3f5159f451b0fb58c
furknbulbul <furknblbl@gmail.com>
2023-10-29 15:12:16 +0300

    Creating and accepting task is implemented

---
+++ resq/backend/resq/src/main/java/com/groupa1/resq/request/CreateTaskRequest.java
@@ -0,0 +1,46 @@
+package com.groupa1.resq.request;
+
+
+import com.groupa1.resq.entity.enums.EGender;
+import com.groupa1.resq.entity.enums.EStatus;
+import com.groupa1.resq.entity.enums.EUrgency;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
+public class CreateTaskRequest {
+    private Long assignerId;
+    private Long assigneeId;
+    private String description;
+    private List<Action> actions;
+    private List<Resource> resources;
+    private EUrgency urgency;
+    private EStatus status;
+
+    @Data
+    public static class Action {
+        private Long verifierId;
+        private String description;
+        private BigDecimal startLatitude;
+        private BigDecimal startLongitude;
+        private BigDecimal endLatitude;
+        private BigDecimal endLongitude;
+    }
+
+    @Data
+    public static class Resource {
+        private Long ownerId;
+        private String categoryTreeId;
+        private EGender gender;
+        private Integer quantity;
+        private BigDecimal latitude;
+        private BigDecimal longitude;
+
+
+    }
+
+
+}
+
