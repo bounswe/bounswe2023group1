8eebcbc2422797cafd9b03de4d575ff19fa33795
Ilgaz Er <er.ilgaz@gmail.com>
2023-10-29 19:21:34 +0300

    create folder structure

--- resq/frontend/src/App.js
+++ resq/frontend/src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import {Navbar, Container, Nav} from 'react-bootstrap';
-import SignIn from "./SignIn";
-import SignUp from "./SignUp";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Test from "./pages/Test";
 
 const smallRedCircle = (
     <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="20"
-      height="20"
-      viewBox="0 0 20 20"
+        xmlns="http://www.w3.org/2000/svg"
+        width="20"
+        height="20"
+        viewBox="0 0 20 20"
     >
-      <circle cx="10" cy="10" r="8" fill="red" />
+        <circle cx="10" cy="10" r="8" fill="red"/>
     </svg>
-  );
+);
 
 const navLinks = [
-    { path: '/signin', label: 'Sign In', component: SignIn, icon: smallRedCircle, },
-    { path: '/signup', label: 'Sign Up', component: SignUp, icon: smallRedCircle, },
-    
+    {path: '/signin', label: 'Sign In', component: SignIn, icon: smallRedCircle,},
+    {path: '/signup', label: 'Sign Up', component: SignUp, icon: smallRedCircle,},
+    {path: '/test', label: "Test", component: Test, icon: smallRedCircle}
 ];
 
 
@@ -28,7 +29,9 @@ function App() {
             <div>
                 <Navbar bg="light" variant="light" expand="lg">
                     <Container>
-                        <Navbar.Brand href="/"><smallRedCircle/> ResQ</Navbar.Brand>
+                        <Navbar.Brand href="/">
+                            <smallRedCircle/>
+                            ResQ</Navbar.Brand>
                         <Navbar.Toggle aria-controls="basic-navbar-nav"/>
                         <Navbar.Collapse id="basic-navbar-nav">
                             <Nav className="me-auto" style={{width: "100%"}}>
@@ -44,7 +47,7 @@ function App() {
                 </Navbar>
                 <main>
                     <Routes>
-                        {navLinks.map(({ path, component }) => (
+                        {navLinks.map(({path, component}) => (
                             <Route key={path} path={path} element={React.createElement(component)}/>
                         ))}
                     </Routes>
--- resq/frontend/src/pages/SignUp.js
+++ resq/frontend/src/pages/SignUp.js
@@ -11,7 +11,7 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import disasterImage from "./disaster.png";
+import disasterImage from "../disaster.png";
 import { useNavigate } from 'react-router-dom';
 
 
