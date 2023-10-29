60f52fbce9efa9aa614ece9d69716e16af9b74bb
furknbulbul <furknblbl@gmail.com>
2023-10-29 15:10:09 +0300

    username is converted to email

--- resq/backend/resq/src/main/java/com/groupa1/resq/auth/UserDetailsServiceImpl.java
+++ resq/backend/resq/src/main/java/com/groupa1/resq/auth/UserDetailsServiceImpl.java
@@ -1,7 +1,6 @@
 package com.groupa1.resq.auth;
 
 import com.groupa1.resq.entity.User;
-import com.groupa1.resq.repository.UserRepository;
 import com.groupa1.resq.service.UserService;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,18 +15,20 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Autowired
     UserService userService;
 
+
+    // Since the methods shoul override the UserDetailsService interface, method name is loadUserByUsername, but it uses email
     @Override
     @Transactional
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userService.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with email: " + email));
 
         return UserDetailsImpl.build(user);
     }
 
-    public UserDetails loadUserByUsername(String username, String selectedRole) {
-        User user = userService.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+    public UserDetails loadUserByUsername(String email, String selectedRole) {
+        User user = userService.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with email: " + email));
 
         return UserDetailsImpl.build(user, selectedRole);
     }
