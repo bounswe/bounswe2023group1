caba8f21b40ed9b89395efb3497a183754f249aa
kubraaksux <hatice.aksu@boun.edu.tr>
2023-10-30 12:55:35 +0300

    conflict resolved

--- resq/frontend/src/App.js
+++ resq/frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import {Navbar, Container, Nav} from 'react-bootstrap';
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
