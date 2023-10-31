6489584f2fa39d476df347331443c13436505391
Çağrı Gülbeycan <87599787+GulbeycanCagri@users.noreply.github.com>
2023-10-31 12:11:48 +0000

    Update ProfileApi.kt

--- resq/mobile/ResQ/app/src/main/java/com/cmpe451/resq/data/remote/ProfileApi.kt
+++ resq/mobile/ResQ/app/src/main/java/com/cmpe451/resq/data/remote/ProfileApi.kt
@@ -1,28 +1,73 @@
 package com.cmpe451.resq.data.remote
 
 import com.cmpe451.resq.data.models.ProfileData
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Header
+
+interface ProfileService {
+    @GET("user/getUserInfo?userId=13")
+    suspend fun getUserInfo(@Header("Authorization") jwtToken :String,
+                            @Header("X-Selected-Role") role: String): Response<UserInfoResponse>
+}
+
+data class UserInfoResponse(
+    val name: String,
+    val surname: String,
+    val email: String,
+    val roles: List<String>
+)
 
 class ProfileRepository {
-    fun getUserData(userId: Int): ProfileData {
-        //(TODO (make an actual API request here))
-        // This is a placeholder response
-/*
-        return ProfileData(
-            name = "Responder",
-            surname = "Responderoğlu",
-            dateOfBirth = "01/01/1990",
-            role = "Responder" ,
-            address = "123 Main Street, apt 4B San Diego CA, 91911"
-            //address = null
+
+    private val profileService: ProfileService
+
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("http://16.16.63.194/resq/api/v1/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        profileService = retrofit.create(ProfileService::class.java)
+    }
+
+    suspend fun getUserData(): ProfileData{
+
+        val response =  profileService.getUserInfo(
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJhbGl0cGMyNTI1QGdtYWlsLmNvbSIsImlhdCI6MTY5ODcwMzU5MiwiZXhwIjoxNjk4Nzg5OTkyfQ.FLyFCITkyCEYOuMBdaSHWCl5V1MrLSl5Yt5Y2L2WAlc",
+            "Victim"
         )
-*/
-
 
         return ProfileData(
-            name = "Harun",
-            surname = "Ergen",
-            dateOfBirth = "18/10/2000",
-            role = "Victim" ,
+            name = response.body()?.name,
+            surname = response.body()?.surname,
+            dateOfBirth = "29/05/1993",
+            role = "Victim",
+            address = ""
             //address = "123 Main Street, apt 4B San Diego CA, 91911"
-           address = null)
+        )
+
+        // This is a placeholder response
+        /*
+                return ProfileData(
+                    name = "Responder",
+                    surname = "Responderoğlu",
+                    dateOfBirth = "01/01/1990",
+                    role = "Responder" ,
+                    address = "123 Main Street, apt 4B San Diego CA, 91911"
+                    //address = null
+                )
+        */
+
+        /*
+                return ProfileData(
+                    name = "Harun",
+                    surname = "Ergen",
+                    dateOfBirth = "18/10/2000",
+                    role = "Victim" ,
+                    //address = "123 Main Street, apt 4B San Diego CA, 91911"
+                   address = null)
+          */
     }}
