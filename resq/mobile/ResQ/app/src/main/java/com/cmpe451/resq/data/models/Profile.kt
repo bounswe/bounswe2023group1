e2ba7dd6fa6c8d9aa0bfbfa345f9af5e3a5f0d47
Çağrı Gülbeycan <87599787+GulbeycanCagri@users.noreply.github.com>
2023-10-31 12:10:22 +0000

    Update Profile.kt

--- resq/mobile/ResQ/app/src/main/java/com/cmpe451/resq/data/models/Profile.kt
+++ resq/mobile/ResQ/app/src/main/java/com/cmpe451/resq/data/models/Profile.kt
@@ -1,10 +1,11 @@
 package com.cmpe451.resq.data.models
 
 open class ProfileData(
-     var name: String,
-     var surname: String,
-     var dateOfBirth: String?,
-     var role: String,
+    var name: String?,
+    var surname: String?,
+    var dateOfBirth: String?,
+    var role: String,
     var address: String?
 ){}
+
 
