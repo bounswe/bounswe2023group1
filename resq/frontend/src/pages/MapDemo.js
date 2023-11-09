aa4b206d6a25eb4346f98c6badf1130dda07538b
kubraaksux <hatice.aksu@boun.edu.tr>
2023-11-09 23:04:05 +0300

    Feature: Integrate Google Reverse Geocoding for Human-Readable Location Display

--- resq/frontend/src/pages/MapDemo.js
+++ resq/frontend/src/pages/MapDemo.js
@@ -1,18 +1,29 @@
 import * as React from 'react';
 import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Box from '@mui/material/Box';
+import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import {createTheme, ThemeProvider} from '@mui/material/styles';
+import disasterImage from '../disaster.png';
 import Container from '@mui/material/Container';
+import {useNavigate} from 'react-router-dom';
 import DisasterMap from "../components/DisasterMap";
-import {useState} from "react";
+import { useState, useEffect } from 'react';
 import {Card, CardActions, CardContent, CardHeader, Collapse, IconButton} from "@mui/material";
 import {type_colors} from "../Colors";
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 import styled from "styled-components";
-
+import axios from 'axios';
+import Geocode from 'react-geocode';
 
 const customTheme = createTheme({
     palette: {
@@ -24,18 +35,18 @@ const customTheme = createTheme({
 
 function Copyright(props) {
     return (
-        <div style={{position: 'fixed', bottom: 0, width: '100%'}}>
-            <Typography variant="body2" color="text.secondary" align="center" {...props}>
-                {'Copyright © '}
-                <Link color="inherit" href="https://github.com/bounswe/bounswe2023group1">
-                    <span style={{fontWeight: 'bold'}}>ResQ</span>
-                </Link>{' '}
-                {new Date().getFullYear()}
-                {'.'}
-            </Typography>
-        </div>
+      <div style={{ position: 'fixed', bottom: 0, width: '100%' }}>
+        <Typography variant="body2" color="text.secondary" align="center" {...props}>
+          {'Copyright © '}
+          <Link color="inherit" href="https://github.com/bounswe/bounswe2023group1">
+            <span style={{ fontWeight: 'bold' }}>ResQ</span>
+          </Link>{' '}
+          {new Date().getFullYear()}
+          {'.'}
+        </Typography>
+      </div>
     );
-}
+  }
 
 
 const ExpandMore = styled(IconButton)`
@@ -50,8 +61,28 @@ const OffsetActions = styled(CardActions)`
   padding: 0;
 `
 
-const RequestCard = ({request: {requester, urgency, needs, status}}) => {
+const RequestCard = ({request: {requester, urgency, needs, status, longitude, latitude }}) => {
     const [expanded, setExpanded] = useState(false);
+    const [locationName, setLocationName] = useState('');
+    const [cityName, setCityName] = useState('');
+
+    useEffect(() => {
+        const reverseGeocode = async () => {
+        try {
+            const response = await axios.get(
+            `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyCehlfJwJ-V_xOWZ9JK3s0rcjkV2ga0DVg`
+            );
+
+            const cityName = response.data.results[0]?.formatted_address || 'Unknown';
+            setLocationName(cityName);
+            setCityName(cityName);
+        } catch (error) {
+            console.error('Error fetching location name:', error);
+        }
+        };
+
+        reverseGeocode();
+    }, [latitude, longitude]);
 
     return <Card variant="outlined">
         <CardHeader
@@ -64,15 +95,16 @@ const RequestCard = ({request: {requester, urgency, needs, status}}) => {
             title={needs.map(({name, quantity}) => `${quantity} ${name}`).join(", ")}
         />
         <CardContent>
-            <Typography variant="body1" sx={{fontSize: '16px', fontWeight: 'bold'}}>
-                Urgency: <span style={{color: 'red', fontWeight: 'bold'}}>{urgency}</span> | Status: <span
-                style={{color: 'red', fontWeight: 'bold'}}>{status}</span>
+            <Typography variant="body1" sx={{ fontSize: '16px', fontWeight: 'bold' }}>
+                Urgency: <span style={{ color: 'red', fontWeight: 'bold' }}>{urgency}</span> | Status: <span style={{ color: 'red' , fontWeight: 'bold'}}>{status}</span>
             </Typography>
 
-            <Typography variant="body2" color="text.primary" sx={{fontSize: '12px', fontWeight: 'bold'}}>
+            <Typography variant="body2" color="text.primary" sx={{ fontSize: '12px', fontWeight: 'bold' }}>
                 Made by: {requester.name} {requester.surname}
             </Typography>
-
+            <Typography variant="body2" color="text.primary" sx={{ fontSize: '12px', fontWeight: 'bold' }}>
+                Location: {`(${cityName})`}
+            </Typography>
         </CardContent>
         <OffsetActions disableSpacing>
             {/*<IconButton aria-label="add to favorites">
@@ -103,9 +135,29 @@ const RequestCard = ({request: {requester, urgency, needs, status}}) => {
 }
 
 
-
-const ResourceCard = ({request: {owner, urgency, resources, status}}) => {
+const ResourceCard = ({request: {owner, urgency, resources, status, longitude, latitude}}) => {
     const [expanded, setExpanded] = useState(false);
+    const [locationName, setLocationName] = useState('');
+    const [cityName, setCityName] = useState('');
+
+    useEffect(() => {
+        const reverseGeocode = async () => {
+        try {
+            const response = await axios.get(
+            `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyCehlfJwJ-V_xOWZ9JK3s0rcjkV2ga0DVg`
+            );
+
+            const cityName = response.data.results[0]?.formatted_address || 'Unknown';
+            setLocationName(cityName);
+            setCityName(cityName);
+        } catch (error) {
+            console.error('Error fetching location name:', error);
+        }
+        };
+
+        reverseGeocode();
+    }, [latitude, longitude]);
+
 
     return <Card variant="outlined">
         <CardHeader
@@ -123,6 +175,9 @@ const ResourceCard = ({request: {owner, urgency, resources, status}}) => {
             </Typography>
             <Typography variant="body2" color="text.primary" sx={{ fontSize: '12px', fontWeight: 'bold' }}>
                 Owner: {owner.name} {owner.surname}
+            </Typography>
+            <Typography variant="body2" color="text.primary" sx={{ fontSize: '12px', fontWeight: 'bold' }}>
+                Location: {`(${cityName})`}
             </Typography>
         </CardContent>
         <OffsetActions disableSpacing>
@@ -223,6 +278,7 @@ const mock_markers = [
 
 export default function MapDemo() {
 
+    const navigate = useNavigate();
     const [selectedPoint, setSelectedPoint] = useState(null)
     const SelectedCard = selectedPoint && cards[selectedPoint.type]
     // noinspection JSValidateTypes
@@ -243,10 +299,12 @@ export default function MapDemo() {
                     </Box>
                 </Box>
 
-                <Copyright sx={{mt: 5}}/>
+                <Copyright sx={{ mt: 5 }} />
             </Container>
         </ThemeProvider>
     );
 }
+
+
 
 
