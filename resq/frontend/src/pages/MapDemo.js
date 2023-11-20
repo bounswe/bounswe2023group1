e4179f9685af550746a855b4799d8135e0beca24
kubraaksux <hatice.aksu@boun.edu.tr>
2023-11-20 03:17:55 +0300

    Add feature to customize row size per page in data table

--- resq/frontend/src/pages/MapDemo.js
+++ resq/frontend/src/pages/MapDemo.js
@@ -9,14 +9,14 @@ import Link from '@mui/material/Link';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import {createTheme, ThemeProvider} from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import disasterImage from '../disaster.png';
 import Container from '@mui/material/Container';
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import DisasterMap from "../components/DisasterMap";
 import { useState, useEffect } from 'react';
-import {Card, CardActions, CardContent, CardHeader, Collapse, IconButton} from "@mui/material";
-import {type_colors} from "../Colors";
+import { Card, CardActions, CardContent, CardHeader, Collapse, IconButton } from "@mui/material";
+import { type_colors } from "../Colors";
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
@@ -35,24 +35,10 @@ const customTheme = createTheme({
     },
 });
 
-function Copyright(props) {
-    return (
-      <div style={{ position: 'fixed', bottom: 0, width: '100%' }}>
-        <Typography variant="body2" color="text.secondary" align="center" {...props}>
-          {'Copyright © '}
-          <Link color="inherit" href="https://github.com/bounswe/bounswe2023group1">
-            <span style={{ fontWeight: 'bold' }}>ResQ</span>
-          </Link>{' '}
-          {new Date().getFullYear()}
-          {'.'}
-        </Typography>
-      </div>
-    );
-  }
 
 
 const ExpandMore = styled(IconButton)`
-  transform: ${({expand}) => !expand ? 'rotate(0deg)' : 'rotate(180deg)'};
+  transform: ${({ expand }) => !expand ? 'rotate(0deg)' : 'rotate(180deg)'};
   margin-left: auto;
   transition: transform 150ms;
 `
@@ -63,24 +49,24 @@ const OffsetActions = styled(CardActions)`
   padding: 0;
 `
 
-const RequestCard = ({request: {requester, urgency, needs, status, longitude, latitude }}) => {
+const RequestCard = ({ request: { requester, urgency, needs, status, longitude, latitude } }) => {
     const [expanded, setExpanded] = useState(false);
     const [locationName, setLocationName] = useState('');
     const [cityName, setCityName] = useState('');
 
     useEffect(() => {
         const reverseGeocode = async () => {
-        try {
-            const response = await axios.get(
-            `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyCehlfJwJ-V_xOWZ9JK3s0rcjkV2ga0DVg`
-            );
+            try {
+                const response = await axios.get(
+                    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyCehlfJwJ-V_xOWZ9JK3s0rcjkV2ga0DVg`
+                );
 
-            const cityName = response.data.results[0]?.formatted_address || 'Unknown';
-            setLocationName(cityName);
-            setCityName(cityName);
-        } catch (error) {
-            console.error('Error fetching location name:', error);
-        }
+                const cityName = response.data.results[0]?.formatted_address || 'Unknown';
+                setLocationName(cityName);
+                setCityName(cityName);
+            } catch (error) {
+                console.error('Error fetching location name:', error);
+            }
         };
 
         reverseGeocode();
@@ -89,16 +75,16 @@ const RequestCard = ({request: {requester, urgency, needs, status, longitude, la
     return <Card variant="outlined">
         <CardHeader
             avatar={
-                <Avatar sx={{bgcolor: type_colors["Request"]}} aria-label="Request">
+                <Avatar sx={{ bgcolor: type_colors["Request"] }} aria-label="Request">
                     Rq
                 </Avatar>
             }
-            titleTypographyProps={{variant: 'h6'}}
-            title={needs.map(({name, quantity}) => `${quantity} ${name}`).join(", ")}
+            titleTypographyProps={{ variant: 'h6' }}
+            title={needs.map(({ name, quantity }) => `${quantity} ${name}`).join(", ")}
         />
         <CardContent>
             <Typography variant="body1" sx={{ fontSize: '16px', fontWeight: 'bold' }}>
-                Urgency: <span style={{ color: 'red', fontWeight: 'bold' }}>{urgency}</span> | Status: <span style={{ color: 'red' , fontWeight: 'bold'}}>{status}</span>
+                Urgency: <span style={{ color: 'red', fontWeight: 'bold' }}>{urgency}</span> | Status: <span style={{ color: 'red', fontWeight: 'bold' }}>{status}</span>
             </Typography>
 
             <Typography variant="body2" color="text.primary" sx={{ fontSize: '12px', fontWeight: 'bold' }}>
@@ -121,12 +107,12 @@ const RequestCard = ({request: {requester, urgency, needs, status, longitude, la
                 aria-expanded={expanded}
                 aria-label="show more"
             >
-                <ExpandMoreIcon/>
+                <ExpandMoreIcon />
             </ExpandMore>
         </OffsetActions>
         <Collapse in={expanded} timeout="auto" unmountOnExit>
             <CardContent>
-                {needs.map(({name, description, quantity}) =>
+                {needs.map(({ name, description, quantity }) =>
                     <Typography variant="body2" color="text.primary">
                         {quantity} {name}: {description}
                     </Typography>
@@ -137,24 +123,24 @@ const RequestCard = ({request: {requester, urgency, needs, status, longitude, la
 }
 
 
-const ResourceCard = ({request: {owner, urgency, resources, status, longitude, latitude}}) => {
+const ResourceCard = ({ request: { owner, urgency, resources, status, longitude, latitude } }) => {
     const [expanded, setExpanded] = useState(false);
     const [locationName, setLocationName] = useState('');
     const [cityName, setCityName] = useState('');
 
     useEffect(() => {
         const reverseGeocode = async () => {
-        try {
-            const response = await axios.get(
-            `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyCehlfJwJ-V_xOWZ9JK3s0rcjkV2ga0DVg`
-            );
+            try {
+                const response = await axios.get(
+                    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=AIzaSyCehlfJwJ-V_xOWZ9JK3s0rcjkV2ga0DVg`
+                );
 
-            const cityName = response.data.results[0]?.formatted_address || 'Unknown';
-            setLocationName(cityName);
-            setCityName(cityName);
-        } catch (error) {
-            console.error('Error fetching location name:', error);
-        }
+                const cityName = response.data.results[0]?.formatted_address || 'Unknown';
+                setLocationName(cityName);
+                setCityName(cityName);
+            } catch (error) {
+                console.error('Error fetching location name:', error);
+            }
         };
 
         reverseGeocode();
@@ -164,16 +150,16 @@ const ResourceCard = ({request: {owner, urgency, resources, status, longitude, l
     return <Card variant="outlined">
         <CardHeader
             avatar={
-                <Avatar sx={{bgcolor: type_colors["Resource"]}} aria-label="Resource">
+                <Avatar sx={{ bgcolor: type_colors["Resource"] }} aria-label="Resource">
                     Rs
                 </Avatar>
             }
-            titleTypographyProps={{variant: 'h6'}}
-            title={resources.map(({name, quantity}) => `${quantity} ${name}`).join(", ")}
+            titleTypographyProps={{ variant: 'h6' }}
+            title={resources.map(({ name, quantity }) => `${quantity} ${name}`).join(", ")}
         />
         <CardContent>
             <Typography variant="body1" sx={{ fontSize: '16px', fontWeight: 'bold' }}>
-                Urgency: <span style={{ color: 'red', fontWeight: 'bold' }}>{urgency}</span> | Status: <span style={{ color: 'blue' , fontWeight: 'bold'}}>{status}</span>
+                Urgency: <span style={{ color: 'red', fontWeight: 'bold' }}>{urgency}</span> | Status: <span style={{ color: 'blue', fontWeight: 'bold' }}>{status}</span>
             </Typography>
             <Typography variant="body2" color="text.primary" sx={{ fontSize: '12px', fontWeight: 'bold' }}>
                 Owner: {owner.name} {owner.surname}
@@ -195,12 +181,12 @@ const ResourceCard = ({request: {owner, urgency, resources, status, longitude, l
                 aria-expanded={expanded}
                 aria-label="show more"
             >
-                <ExpandMoreIcon/>
+                <ExpandMoreIcon />
             </ExpandMore>
         </OffsetActions>
         <Collapse in={expanded} timeout="auto" unmountOnExit>
             <CardContent>
-                {resources.map(({name, description, quantity}) =>
+                {resources.map(({ name, description, quantity }) =>
                     <Typography variant="body2" color="text.primary">
                         {quantity} {name}: {description}
                     </Typography>
@@ -287,25 +273,24 @@ export default function MapDemo() {
     return (
         <ThemeProvider theme={customTheme}>
             <Container component="main" maxWidth="100%">
-                <CssBaseline/>
-                <Box sx={{display: "flex", flexDirection: "row", flexWrap: 'nowrap', margin: "12px"}}>
+                <CssBaseline />
+                <Box sx={{ display: "flex", flexDirection: "row", flexWrap: 'nowrap', margin: "12px" }}>
                     {selectedPoint && <>
-                        <Box sx={{flexBasis: "33%", flexShrink: 0}}>
-                            <SelectedCard request={selectedPoint}/>
+                        <Box sx={{ flexBasis: "33%", flexShrink: 0 }}>
+                            <SelectedCard request={selectedPoint} />
                         </Box>
-                        <Box sx={{width: "36px"}}/>
+                        <Box sx={{ width: "36px" }} />
                     </>
                     }
-                    {(!selectedPoint) && <> 
-                        <MapDataGrid sx={{flexBasis: "33%", flexShrink: 0, display: "flex", flexDirection: "row", flexWrap: 'nowrap', marginRight: "12px"}}/>
-                        <Box sx={{ width: "46px" }}/>
+                    {(!selectedPoint) && <>
+                        <MapDataGrid sx={{ flexBasis: "33%", flexShrink: 0, display: "flex", flexDirection: "row", flexWrap: 'nowrap', marginRight: "12px" }} />
+                        <Box sx={{ width: "46px" }} />
                     </>}
-                    <Box sx={{flexBasis: "66%", flexGrow: 100}}>
-                        <DisasterMap markers={mock_markers} onPointSelected={setSelectedPoint}/>
+                    <Box sx={{ flexBasis: "66%", flexGrow: 100 }}>
+                        <DisasterMap markers={mock_markers} onPointSelected={setSelectedPoint} />
                     </Box>
                 </Box>
 
-                <Copyright sx={{ mt: 5 }} />
             </Container>
         </ThemeProvider>
     );
