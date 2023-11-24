a372bc5f7c5e371f4ab1bae7dfdf2202c713f86d
Ilgaz Er <er.ilgaz@gmail.com>
2023-11-24 03:16:28 +0300

    add notifications and its page

--- resq/frontend/src/App.js
+++ resq/frontend/src/App.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import React, {useState} from 'react';
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+import {Navbar, Container, Nav} from 'react-bootstrap';
 import UserRoles from "./pages/UserRoles";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
@@ -8,7 +8,11 @@ import MapDemo from "./pages/MapDemo";
 import Account from "./pages/Account";
 import RoleRequest from "./pages/RoleRequest";
 import LogoutIcon from '@mui/icons-material/Logout';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 import Request from "./pages/RequestCreation";
+import {Badge} from "@mui/material";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
+import Notifications from "./pages/Notifications";
 
 const SmallRedCircle = () =>
     <svg
@@ -17,30 +21,42 @@ const SmallRedCircle = () =>
         height="20"
         viewBox="0 0 20 20"
     >
-        <circle cx="10" cy="10" r="8" fill="red" />
+        <circle cx="10" cy="10" r="8" fill="red"/>
     </svg>
 
 function App() {
     const [token, _setToken] = useState(localStorage.getItem("token"))
     const [role, setRole] = useState("")
+    const [notifications, setNotifications] = useState([
+        {
+            title: "Ongoing fire on Cami Sk.",
+            desc: "A fire has been reported on Rumeli Hisarı Mh, Cami Sk, Sarıyer/İstanbul.",
+            read: false
+        },{
+            title: "New Soup Kitchen Near You",
+            desc: "A soup kitchen has opened on Etiler, Hisar Üstü Nispetiye Cd No:1, Beşiktaş/İstanbul.",
+            read: true
+        }
+    ])
+
     const setToken = t => {
         localStorage.setItem("token", t || "")
         _setToken(t)
     }
 
     const navLinks = [
-        { path: '/map', label: <strong>Map Demo</strong>, component: MapDemo, icon: <SmallRedCircle /> },
+        {path: '/map', label: <strong>Map Demo</strong>, component: MapDemo, icon: <SmallRedCircle/>},
         token && {
             path: '/userroles',
             label: <strong>User Roles</strong>,
             component: UserRoles,
-            icon: <SmallRedCircle />
+            icon: <SmallRedCircle/>
         },
         (role === "responder") && {
             path: '/responder',
             label: <strong>Responder Panel</strong>,
             component: <div>Responder Panel</div>,
-            icon: <SmallRedCircle />
+            icon: <SmallRedCircle/>
         },
     ].filter(l => !!l);
 
@@ -48,81 +64,103 @@ function App() {
         setToken(null)
     }
 
+    const theme = createTheme({
+        palette: {
+            primary: {
+                main: "#ff0000"
+            },
+            secondary: {
+                main: '#00f0ff'
+            }
+        }
+    });
+
     return (
-        <Router>
-            <div>
-                <Navbar bg="light" variant="light" expand="lg">
-                    <Container>
-                        <Navbar.Brand href="/" style={{ color: 'red', fontWeight: 'bold' }}>
-                            <SmallRedCircle />
-                            ResQ
-                        </Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="me-auto">
-                                {navLinks.map(({ path, label, icon }) => (
-                                    <Nav.Link key={path} href={path}>
-                                        {icon}
-                                        {label}
-                                    </Nav.Link>
-                                ))}
-                            </Nav>
-                            <Nav className="ml-auto">
-                                {token ?
-                                    <>
-                                        <Nav.Link key={"/account"} href={"/account"} style={{ "marginLeft": "auto" }}>
-                                            <SmallRedCircle />
-                                            <strong>Account</strong>
+        <ThemeProvider theme={theme}>
+            <Router>
+                <div>
+                    <Navbar bg="light" variant="light" expand="lg">
+                        <Container>
+                            <Navbar.Brand href="/" style={{color: 'red', fontWeight: 'bold'}}>
+                                <SmallRedCircle/>
+                                ResQ
+                            </Navbar.Brand>
+                            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                            <Navbar.Collapse id="basic-navbar-nav">
+                                <Nav className="me-auto">
+                                    {navLinks.map(({path, label, icon}) => (
+                                        <Nav.Link key={path} href={path}>
+                                            {icon}
+                                            {label}
                                         </Nav.Link>
-                                        <Nav.Link key={"/requestcreation"} href={"/requestcreation"} style={{ "marginLeft": "auto" }}>
-                                            <SmallRedCircle />
-                                            <strong>Create Request</strong>
-                                        </Nav.Link>
-                                        <Nav.Link key={"signout"} href={"#"} onClick={signOut}
-                                            style={{ "marginLeft": "auto" }}>
-                                            <LogoutIcon />
-                                        </Nav.Link>
-                                    </> :
-                                    <>
-                                        <Nav.Link key={'/signin'} href={'/signin'} style={{ "marginLeft": "auto" }}>
-                                            <strong>Sign In</strong>
-                                        </Nav.Link>
-                                        <Nav.Link key={'/signup'} href={'/signup'} style={{ "marginLeft": "auto" }}>
-                                            <strong>Sign Up</strong>
-                                        </Nav.Link>
+                                    ))}
+                                </Nav>
+                                <Nav className="ml-auto">
+                                    {token ?
+                                        <>
+                                            <Nav.Link key={"/account"} href={"/account"} style={{"marginLeft": "auto"}}>
+                                                <SmallRedCircle/>
+                                                <strong>Account</strong>
+                                            </Nav.Link>
+                                            <Nav.Link key={"/requestcreation"} href={"/requestcreation"}
+                                                      style={{"marginLeft": "auto"}}>
+                                                <SmallRedCircle/>
+                                                <strong>Create Request</strong>
+                                            </Nav.Link>
+                                            <Nav.Link key={"/notifications"} href={"/notifications"}
+                                                      style={{"marginLeft": "auto"}}>
+                                                <Badge badgeContent={notifications.filter(n => !n?.read).length}
+                                                       color={"primary"}>
+                                                    <NotificationsIcon/>
+                                                </Badge>
+                                            </Nav.Link>
+                                            <Nav.Link key={"signout"} href={"#"} onClick={signOut}>
+                                                <LogoutIcon/>
+                                            </Nav.Link>
+                                        </> :
+                                        <>
+                                            <Nav.Link key={'/signin'} href={'/signin'} style={{"marginLeft": "auto"}}>
+                                                <strong>Sign In</strong>
+                                            </Nav.Link>
+                                            <Nav.Link key={'/signup'} href={'/signup'} style={{"marginLeft": "auto"}}>
+                                                <strong>Sign Up</strong>
+                                            </Nav.Link>
+                                        </>
+                                    }
+                                </Nav>
+                            </Navbar.Collapse>
+                        </Container>
+                    </Navbar>
+                    <main>
+                        <Routes>
+                            {navLinks.map(({path, component}) => (
+                                <Route key={path} path={path}
+                                       element={React.createElement(component, {token, setToken, role, setRole})}/>
+                            ))}
+                            <Route path="/" element={<Navigate to="/map"/>}/>
+                            <Route path="/rolerequest" state={{token, setToken}}
+                                   element={React.createElement(RoleRequest, {token, setToken})}/>
+                            {
+                                token ? <>
+                                        <Route path="/account" state={{token, setToken}}
+                                               element={React.createElement(Account, {token, setToken})}/>
+                                        <Route path="/requestcreation" state={{token, setToken}}
+                                               element={React.createElement(Request, {token, setToken})}/>
+                                        <Route path="/notifications" state={{token, setToken}}
+                                               element={React.createElement(Notifications, {token, notifications, setNotifications})}/>
                                     </>
-                                }
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-                <main>
-                    <Routes>
-                        {navLinks.map(({ path, component }) => (
-                            <Route key={path} path={path}
-                                element={React.createElement(component, { token, setToken, role, setRole })} />
-                        ))}
-                        <Route path="/" element={<Navigate to="/map" />} />
-                        <Route path="/rolerequest" state={{ token, setToken }}
-                            element={React.createElement(RoleRequest, { token, setToken })} />
-                        {
-                            token ? <>
-                                <Route path="/account" state={{ token, setToken }}
-                                    element={React.createElement(Account, { token, setToken })} />
-                                <Route path="/requestcreation" state={{ token, setToken }}
-                                    element={React.createElement(Request, { token, setToken })} />
-                            </>
-                                : <>
-                                    <Route path="/signin" state={{ token, setToken }}
-                                        element={React.createElement(SignIn, { token, setToken })} />
-                                    <Route path="/signup" state={{ token, setToken }}
-                                        element={React.createElement(SignUp, { token, setToken })} />
-                                </>
-                        }
-                    </Routes>
-                </main>
-            </div>
-        </Router>
+                                    : <>
+                                        <Route path="/signin" state={{token, setToken}}
+                                               element={React.createElement(SignIn, {token, setToken})}/>
+                                        <Route path="/signup" state={{token, setToken}}
+                                               element={React.createElement(SignUp, {token, setToken})}/>
+                                    </>
+                            }
+                        </Routes>
+                    </main>
+                </div>
+            </Router>
+        </ThemeProvider>
     );
 }
 
---
+++ resq/frontend/src/pages/Notifications.js
@@ -0,0 +1,85 @@
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import {createTheme, ThemeProvider} from '@mui/material/styles';
+import Container from '@mui/material/Container';
+import {useNavigate} from 'react-router-dom';
+import {Card, CardContent, CardHeader} from "@mui/material";
+
+
+function Copyright(props) {
+    return (
+        <div style={{position: 'fixed', bottom: 0, width: '100%'}}>
+            <Typography variant="body2" color="text.secondary" align="center" {...props}>
+                {'Copyright © '}
+                <Link color="inherit" href="https://github.com/bounswe/bounswe2023group1">
+                    <span style={{fontWeight: 'bold'}}>ResQ</span>
+                </Link>{' '}
+                {new Date().getFullYear()}
+                {'.'}
+            </Typography>
+        </div>
+    );
+}
+
+const customTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#FF0000',
+        },
+    },
+});
+
+const NotificationCard = ({notif, onClick}) => <Card
+    sx={{marginBottom: "24px", backgroundColor: notif.read ? "lightgrey" : "white"}}
+    onClick={onClick}
+>
+    <CardHeader title={notif.title}/>
+    <CardContent>
+        <Typography variant="body2" color="text.primary" sx={{fontSize: '16px'}}>
+            {notif.desc}
+        </Typography>
+    </CardContent>
+</Card>
+
+export default function Notifications({token, notifications, setNotifications}) {
+
+    const [selectedRole, setSelectedRole] = React.useState('');
+
+    const handleRoleChange = (event) => {
+        setSelectedRole(event.target.value);
+    };
+
+    const navigate = useNavigate();
+
+    const handleNotifRead = (i) => {
+        const new_notifs = JSON.parse(JSON.stringify(notifications))
+        new_notifs[i].read = true
+        setNotifications(new_notifs)
+    }
+
+    return (
+        <ThemeProvider theme={customTheme}>
+            <Container component="main" maxWidth="xs">
+                <CssBaseline/>
+                <Box
+                    sx={{
+                        marginTop: 1,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    {
+                        notifications.map((notif, i) => <NotificationCard notif={notif} onClick={() => notif.read || handleNotifRead(i)}/>)
+                    }
+
+                </Box>
+            </Container>
+            <Copyright sx={{mt: 5}}/>
+        </ThemeProvider>
+    );
+}
+
