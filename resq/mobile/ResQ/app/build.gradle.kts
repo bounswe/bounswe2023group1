412d621a0248f0fb82879749e81c88e84b4b32e1
Çağrı Gülbeycan <87599787+GulbeycanCagri@users.noreply.github.com>
2023-11-26 23:16:49 +0000

    Update build.gradle.kts
    Added coil dependency for implementing profile photo

--- resq/mobile/ResQ/app/build.gradle.kts
+++ resq/mobile/ResQ/app/build.gradle.kts
@@ -94,5 +94,7 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    // Coil
+    implementation("io.coil-kt:coil-compose:2.4.0")
 
 }
