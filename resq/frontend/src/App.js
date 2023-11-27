542a34f94c830a155890377289088a53cd64d918
Ilgaz Er <er.ilgaz@gmail.com>
2023-11-28 01:43:28 +0300

    fix app js

--- resq/frontend/src/App.js
+++ resq/frontend/src/App.js
@@ -1,23 +1,22 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import React, {useEffect, useRef, useState} from 'react';
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+import {Navbar, Container, Nav} from 'react-bootstrap';
 import UserRoles from "./pages/UserRoles";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
-import MapPage from "./pages/MapPage";
 import Account from "./pages/Account";
 import RoleRequest from "./pages/RoleRequest";
 import LogoutIcon from '@mui/icons-material/Logout';
 import NotificationsIcon from '@mui/icons-material/Notifications';
 import Request from "./components/Request/RequestCreation";
 import Resource from "./components/Resource/ResourceCreation";
-import { LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {LocalizationProvider} from "@mui/x-date-pickers";
+import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 import axios from "axios";
 import VictimMapPage from "./pages/VictimMapPage";
-import { Badge } from "@mui/material";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
+import {Badge} from "@mui/material";
+import {createTheme, ThemeProvider} from "@mui/material/styles";
 import Notifications from "./pages/Notifications";
 
 const SmallRedCircle = () =>
@@ -27,7 +26,7 @@ const SmallRedCircle = () =>
         height="20"
         viewBox="0 0 20 20"
     >
-        <circle cx="10" cy="10" r="8" fill="red" />
+        <circle cx="10" cy="10" r="8" fill="red"/>
     </svg>
 
 const queryClient = new QueryClient()
@@ -65,18 +64,18 @@ function App() {
     }
 
     const navLinks = [
-        { path: '/map', label: <strong>Map Demo</strong>, component: VictimMapPage, icon: <SmallRedCircle /> },
+        {path: '/map', label: <strong>Map Demo</strong>, component: VictimMapPage, icon: <SmallRedCircle/>},
         token && {
             path: '/userroles',
             label: <strong>User Roles</strong>,
             component: UserRoles,
-            icon: <SmallRedCircle />
+            icon: <SmallRedCircle/>
         },
         (role === "responder") && {
             path: '/responder',
             label: <strong>Responder Panel</strong>,
             component: <div>Responder Panel</div>,
-            icon: <SmallRedCircle />
+            icon: <SmallRedCircle/>
         },
     ].filter(l => !!l);
 
@@ -100,122 +99,114 @@ function App() {
     return (
         <QueryClientProvider client={queryClient}>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <Router>
-                    <div>
-                        <Navbar bg="light" variant="light" expand="lg">
-                            <Container ref={ref}>
-                                <Navbar.Brand href="/" style={{ color: 'red', fontWeight: 'bold' }}>
-                                    <SmallRedCircle />
-                                    ResQ
-                                </Navbar.Brand>
-                                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                                <Navbar.Collapse id="basic-navbar-nav">
-                                    <Nav className="me-auto">
-                                        {navLinks.map(({ path, label, icon }) => (
-                                            <Nav.Link key={path} href={path}>
-                                                {icon}
-                                                {label}
-                                            </Nav.Link>
-                                        ))}
-                                    </Nav>
-                                    <Nav className="ml-auto">
-                                        {token ?
-                                            <>
-                                                <Nav.Link key={"/account"} href={"/account"}
-                                                    style={{ "marginLeft": "auto" }}>
-                                                    <SmallRedCircle />
-                                                    <strong>Account</strong>
+                <ThemeProvider theme={theme}>
+                    <Router>
+                        <div>
+                            <Navbar bg="light" variant="light" expand="lg">
+                                <Container ref={ref}>
+                                    <Navbar.Brand href="/" style={{color: 'red', fontWeight: 'bold'}}>
+                                        <SmallRedCircle/>
+                                        ResQ
+                                    </Navbar.Brand>
+                                    <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                                    <Navbar.Collapse id="basic-navbar-nav">
+                                        <Nav className="me-auto">
+                                            {navLinks.map(({path, label, icon}) => (
+                                                <Nav.Link key={path} href={path}>
+                                                    {icon}
+                                                    {label}
                                                 </Nav.Link>
-                                                <Nav.Link key={"/requestcreation"} href={"/requestcreation"}
-                                                    style={{ "marginLeft": "auto" }}>
-                                                    <SmallRedCircle />
-                                                    <strong>Create Request</strong>
-                                                </Nav.Link>
-                                                <Nav.Link key={"/resourcecreation"} href={"/resourcecreation"}
-                                                    style={{ "marginLeft": "auto" }}>
-                                                    <SmallRedCircle />
-                                                    <strong>Create Resource</strong>
-                                                </Nav.Link>
-                                                <Nav.Link key={"signout"} href={"#"} onClick={signOut}
-                                                    style={{ "marginLeft": "auto" }}>
-                                                    <LogoutIcon />
-                                                </Nav.Link>
-                                            </> :
-                                            <>
-                                                <Nav.Link key={'/signin'} href={'/signin'}
-                                                    style={{ "marginLeft": "auto" }}>
-                                                    <strong>Sign In</strong>
-                                                </Nav.Link>
-                                                <Nav.Link key={'/signup'} href={'/signup'}
-                                                    style={{ "marginLeft": "auto" }}>
-                                                    <strong>Sign Up</strong>
-                                                </Nav.Link>
+                                            ))}
+                                        </Nav>
+                                        <Nav className="ml-auto">
+                                            {token ?
+                                                <>
+                                                    <Nav.Link key={"/account"} href={"/account"}
+                                                              style={{"marginLeft": "auto"}}>
+                                                        <SmallRedCircle/>
+                                                        <strong>Account</strong>
+                                                    </Nav.Link>
+                                                    <Nav.Link key={"/requestcreation"} href={"/requestcreation"}
+                                                              style={{"marginLeft": "auto"}}>
+                                                        <SmallRedCircle/>
+                                                        <strong>Create Request</strong>
+                                                    </Nav.Link>
+                                                    <Nav.Link key={"/resourcecreation"} href={"/resourcecreation"}
+                                                              style={{"marginLeft": "auto"}}>
+                                                        <SmallRedCircle/>
+                                                        <strong>Create Resource</strong>
+                                                    </Nav.Link>
+                                                    <Nav.Link key={"/notifications"} href={"/notifications"}
+                                                              style={{"marginLeft": "auto"}}>
+                                                        <Badge badgeContent={notifications.filter(n => !n?.read).length}
+                                                               color={"primary"}>
+                                                            <NotificationsIcon/>
+                                                        </Badge>
+                                                    </Nav.Link>
+                                                    <Nav.Link key={"signout"} href={"#"} onClick={signOut}
+                                                              style={{"marginLeft": "auto"}}>
+                                                        <LogoutIcon/>
+                                                    </Nav.Link>
+                                                </> :
+                                                <>
+                                                    <Nav.Link key={'/signin'} href={'/signin'}
+                                                              style={{"marginLeft": "auto"}}>
+                                                        <strong>Sign In</strong>
+                                                    </Nav.Link>
+                                                    <Nav.Link key={'/signup'} href={'/signup'}
+                                                              style={{"marginLeft": "auto"}}>
+                                                        <strong>Sign Up</strong>
+                                                    </Nav.Link>
+                                                </>
+                                            }
+                                        </Nav>
+                                    </Navbar.Collapse>
+                                </Container>
+                            </Navbar>
+
+                            <main style={{height: `${height - 57}px`}}>
+                                <Routes>
+                                    {navLinks.map(({path, component}) => (
+                                        <Route key={path} path={path}
+                                               element={React.createElement(component, {
+                                                   token,
+                                                   setToken,
+                                                   role,
+                                                   setRole
+                                               })}/>
+                                    ))}
+                                    <Route path="/" element={<Navigate to="/map"/>}/>
+                                    <Route path="/rolerequest" state={{token, setToken}}
+                                           element={React.createElement(RoleRequest, {token, setToken})}/>
+                                    {
+                                        token ? <>
+                                                <Route path="/account" state={{token, setToken}}
+                                                       element={React.createElement(Account, {token, setToken})}/>
+                                                <Route path="/requestcreation" state={{token, setToken}}
+                                                       element={React.createElement(Request, {token, setToken})}/>
+                                                <Route path="/notifications" state={{token, setToken}}
+                                                       element={React.createElement(Notifications, {
+                                                           token,
+                                                           notifications,
+                                                           setNotifications
+                                                       })}/>
+                                                <Route path="/resourcecreation" state={{token, setToken}}
+                                                       element={React.createElement(Resource, {token, setToken})}/>
                                             </>
-                                        }
-                                    </Nav>
-                                </Navbar.Collapse>
-                            </Container>
-                        </Navbar>
-                        <main style={{ height: `${height - 57}px` }}>
-                            <Routes>
-                                {navLinks.map(({ path, component }) => (
-                                    <Route key={path} path={path}
-                                        element={React.createElement(component, { token, setToken, role, setRole })} />
-                                ))}
-                                <Route path="/" element={<Navigate to="/map" />} />
-                                <Route path="/rolerequest" state={{ token, setToken }}
-                                    element={React.createElement(RoleRequest, { token, setToken })} />
-                                {
-                                    token ? <>
-                                        <Route path="/account" state={{ token, setToken }}
-                                            element={React.createElement(Account, { token, setToken })} />
-                                        <Route path="/requestcreation" state={{ token, setToken }}
-                                            element={React.createElement(Request, { token, setToken })} />
-                                        <Route path="/resourcecreation" state={{ token, setToken }}
-                                            element={React.createElement(Request, { token, setToken })} />
-                                    </>
-                                        : <>
-                                            <Route path="/signin" state={{ token, setToken }}
-                                                element={React.createElement(SignIn, { token, setToken })} />
-                                            <Route path="/signup" state={{ token, setToken }}
-                                                element={React.createElement(SignUp, { token, setToken })} />
-                                        </>
-                                }
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-                <main style={{ height: `${height - 57}px` }}>
-                    <Routes>
-                        {navLinks.map(({ path, component }) => (
-                            <Route key={path} path={path}
-                                element={React.createElement(component, { token, setToken, role, setRole })} />
-                        ))}
-                        <Route path="/" element={<Navigate to="/map" />} />
-                        <Route path="/rolerequest" state={{ token, setToken }}
-                            element={React.createElement(RoleRequest, { token, setToken })} />
-                        {
-                            token ? <>
-                                <Route path="/account" state={{ token, setToken }}
-                                    element={React.createElement(Account, { token, setToken })} />
-                                <Route path="/requestcreation" state={{ token, setToken }}
-                                    element={React.createElement(Request, { token, setToken })} />
-                                <Route path="/resourcecreation" state={{ token, setToken }}
-                                    element={React.createElement(Resource, { token, setToken })} />
-                            </>
-                                : <>
-                                    <Route path="/signin" state={{ token, setToken }}
-                                        element={React.createElement(SignIn, { token, setToken })} />
-                                    <Route path="/signup" state={{ token, setToken }}
-                                        element={React.createElement(SignUp, { token, setToken })} />
-                                </>
-                        }
-                    </Routes>
-                </main>
-            </div>
-        </Router>
-        </QueryClientProvider >
-    );
+                                            : <>
+                                                <Route path="/signin" state={{token, setToken}}
+                                                       element={React.createElement(SignIn, {token, setToken})}/>
+                                                <Route path="/signup" state={{token, setToken}}
+                                                       element={React.createElement(SignUp, {token, setToken})}/>
+                                            </>
+                                    }
+                                </Routes>
+                            </main>
+                        </div>
+                    </Router>
+                </ThemeProvider>
+            </LocalizationProvider>
+        </QueryClientProvider>);
 }
 
 export default App;
