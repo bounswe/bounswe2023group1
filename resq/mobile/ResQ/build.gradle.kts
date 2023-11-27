7d8a77c494b49b2401c1bad2d6ed7b2f5dd30c8b
Alperen Dağı <111731140+alperenDagi@users.noreply.github.com>
2023-11-27 21:11:55 +0300

    Update build.gradle.kts

--- resq/mobile/ResQ/build.gradle.kts
+++ resq/mobile/ResQ/build.gradle.kts
@@ -7,6 +7,5 @@ plugins {
 buildscript {
     dependencies {
         classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
-        classpath ("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
     }
 }
