a59030ec78081831b7f20819382359e2b8e9013b
Kübra Aksu <kubra_4199@hotmail.com>
2023-12-12 15:58:41 +0300

    Responder specific map page implementat,on first draft

---
+++ resq/frontend/src/pages/ResponderMapPage.js
@@ -0,0 +1,13 @@
+import MapPage from "./MapPage";
+import {useQuery} from "@tanstack/react-query";
+import {getAllResources} from "../AppService";
+import {mock_markers} from "./Mock_markers";
+import {Fab} from "@mui/material";
+import {useNavigate} from "react-router-dom";
+import {Add} from "@mui/icons-material";
+
+
+export default function ResponderMapPage() {
+
+    
+}
