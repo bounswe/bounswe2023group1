1c85f47ea8edf0fbd05a60fe04acd84cab15d677
Kübra Aksu <kubra_4199@hotmail.com>
2023-12-14 00:43:38 +0300

    second draft implementation for responders

--- resq/frontend/src/pages/ResponderMapPage.js
+++ resq/frontend/src/pages/ResponderMapPage.js
@@ -10,11 +10,35 @@ import {Add} from "@mui/icons-material";
 export default function ResponderMapPage() {
 
     
+//const queryClient = useQueryClient()
 
+const navigate = useNavigate()
+const requests = useQuery({queryKey: ['viewAllNeeds'], queryFn: viewAllNeeds})
 
+const requestMarkers = (requests.data?.data || []).map(a => ({...a, type: "Requests"}))
+const allMarkers = [...mock_markers, ...requestMarkers]
+/*/ Mutations
+const mutation = useMutation({
+    mutationFn: postTodo,
+    onSuccess: () => {
+        // Invalidate and refetch
+        queryClient.invalidateQueries({queryKey: ['todos']})
+    },
+})*/
 
+return (
+    <>
+        <MapPage allMarkers={allMarkers}/>
+        <Fab color="primary" variant="extended" onClick={() => navigate("/requestcreation")} sx={{
+            position: 'absolute',
+            bottom: 32,
+            right: 32,
+        }}>
+            <Add sx={{mr: 1}}/>
+            Add Resource
+        </Fab>
+    </>
 
+)
 
-
-    
 }
