45767c6e387b28803a7363b02b6337b9bc42115e
Your GitHub Username <cagri.gulbeycan@boun.edu.tr>
2023-12-24 09:28:20 +0000

    My requests page added

---
+++ resq/mobile/ResQ/app/src/main/java/com/cmpe451/resq/viewmodels/MyRequestsViewModel.kt
@@ -0,0 +1,52 @@
+package com.cmpe451.resq.viewmodels
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.cmpe451.resq.data.models.CategoryTreeNode
+import com.cmpe451.resq.data.models.Need
+import com.cmpe451.resq.data.remote.ResqService
+import kotlinx.coroutines.launch
+import android.content.Context
+
+class MyRequestsViewModel : ViewModel() {
+    val needs = mutableStateOf<List<Need>>(emptyList())
+    private val _categoryTree = mutableStateOf<List<CategoryTreeNode>>(emptyList())
+    suspend fun getNeeds(appContext: Context) {
+        val api = ResqService(appContext)
+        api.viewNeedsByUserId(
+            onSuccess = { needList ->
+                needs.value = needList
+                fetchCategoryTree(appContext)
+            },
+            onError = { error ->
+                // Handle error
+            }
+        )
+    }
+
+    private fun fetchCategoryTree(context: Context) {
+        val api = ResqService(context)
+        viewModelScope.launch {
+            val response = api.getMainCategories() // Assuming this method exists and fetches the entire category tree
+            if (response.isSuccessful) {
+                _categoryTree.value = response.body() ?: emptyList()
+            } else {
+                // Handle error
+            }
+        }
+    }
+
+    fun getCategoryName(categoryId: Int): String {
+        return findCategoryName(_categoryTree.value, categoryId)
+    }
+
+    private fun findCategoryName(categoryList: List<CategoryTreeNode>, categoryId: Int): String {
+        for (category in categoryList) {
+            if (category.id == categoryId) return category.data
+            val foundName = findCategoryName(category.children, categoryId)
+            if (foundName.isNotEmpty() && foundName != "Unknown Category") return foundName
+        }
+        return "Unknown Category"
+    }
+}
