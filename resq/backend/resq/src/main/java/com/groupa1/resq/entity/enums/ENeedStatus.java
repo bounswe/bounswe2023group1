28592a17b1f0f76c81455458d588167776760bcd
furknbulbul <furknblbl@gmail.com>
2023-12-25 19:21:11 +0300

    need status class rolled back

--- resq/backend/resq/src/main/java/com/groupa1/resq/entity/enums/ENeedStatus.java
+++ resq/backend/resq/src/main/java/com/groupa1/resq/entity/enums/ENeedStatus.java
@@ -1,7 +1,7 @@
 package com.groupa1.resq.entity.enums;
 
 public enum ENeedStatus {
-    INVOLVED_REQUEST, // involved means that the need is part of a request
+    INVOLVED, // involved means that the need is part of a request
     NOT_INVOLVED,
     CANCELLED,
     PROVIDED
