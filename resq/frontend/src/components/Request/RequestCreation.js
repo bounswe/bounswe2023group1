e3d7a5dd790cf81d9a824097664b2a7c3005a45e
Kübra Aksu <kubra_4199@hotmail.com>
2023-12-26 10:23:34 +0300

    lines for bug fixes

--- resq/frontend/src/components/Request/RequestCreation.js
+++ resq/frontend/src/components/Request/RequestCreation.js
@@ -1,24 +1,11 @@
-import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Toolbar from '@mui/material/Toolbar';
-import Paper from '@mui/material/Paper';
-import Stepper from '@mui/material/Stepper';
-import Step from '@mui/material/Step';
-import StepLabel from '@mui/material/StepLabel';
-import Button from '@mui/material/Button';
-import Link from '@mui/material/Link';
-import Typography from '@mui/material/Typography';
+import React, { useState } from 'react';
+import { CssBaseline, AppBar, Box, Container, Toolbar, Paper, Stepper, Step, StepLabel, Button, Link, Typography, createTheme, ThemeProvider } from '@mui/material';
 import RequestAddress from './RequestAddress';
 import RequestDetails1 from './RequestDetails1';
 import RequestDetails2 from './RequestDetails2';
 import RequestDetails3 from './RequestDetails3';
 import RequestReview from './RequestReview';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { NeedProvider as MyNeedProvider } from './NeedContext';
-import { useState } from 'react';
+import { NeedProvider } from './NeedContext';
 import { createNeed } from '../../AppService';
 
 function Copyright(props) {
@@ -26,150 +13,94 @@ function Copyright(props) {
         <div style={{ position: 'fixed', bottom: 0, width: '100%' }}>
             <Typography variant="body2" color="text.secondary" align="center" {...props}>
                 {'Copyright © '}
-                <Link color="inherit" href="https://github.com/bounswe/bounswe2023group1">
-                    <span style={{ fontWeight: 'bold' }}>ResQ</span>
-                </Link>{' '}
-                {new Date().getFullYear()}
-                {'.'}
+                <Link color="inherit" href="https://github.com/bounswe/bounswe2023group1">ResQ</Link>{' '}
+                {new Date().getFullYear()}.
             </Typography>
         </div>
     );
 }
 
 const customTheme = createTheme({
-    palette: {
-        primary: {
-            main: '#FF0000',
-        },
-    },
+    palette: { primary: { main: '#FF0000' } },
 });
 
-
 export default function Request() {
-    const [activeStep, setActiveStep] = React.useState(0);
-    const [needData, setNeedData] = useState({
-        senderId: parseInt(localStorage.getItem('userId')),
-        gender: "MALE"
-    });
-
+    const [activeStep, setActiveStep] = useState(0);
+    const [needData, setNeedData] = useState({ senderId: parseInt(localStorage.getItem('userId')), gender: "MALE" });
     const steps = ['Request address', 'Recurrent Needs', 'Clothing Needs', 'Further Needs'];
 
     function getStepContent(step) {
         switch (step) {
-            case 0:
-                return <RequestAddress needData={needData} setNeedData={setNeedData} />;
-            case 1:
-                return <RequestDetails1 needData={needData} setNeedData={setNeedData} />;
-            case 2:
-                return <RequestDetails2 needData={needData} setNeedData={setNeedData} />;
-            case 3:
-                return <RequestDetails3 needData={needData} setNeedData={setNeedData} />;
-            default:
-                throw new Error('Unknown step');
+            case 0: return <RequestAddress needData={needData} setNeedData={setNeedData} />;
+            case 1: return <RequestDetails1 needData={needData} setNeedData={setNeedData} />;
+            case 2: return <RequestDetails2 needData={needData} setNeedData={setNeedData} />;
+            case 3: return <RequestDetails3 needData={needData} setNeedData={setNeedData} />;
+            default: throw new Error('Unknown step');
         }
     }
 
     const handleNext = async () => {
         if (activeStep === steps.length - 1) {
             try {
-                const commonData = {
-                    latitude: needData.latitude,
-                    longitude: needData.longitude,
-                };
+                const commonData = { latitude: needData.latitude, longitude: needData.longitude };
 
-                // Handling water request
                 if (needData.water) {
-                    const waterRequestData = {
-                        ...commonData,
-                        description: "water",
-                        categoryTreeId: needData.waterCategoryTreeId,
-                        quantity: needData.totalPeople,
-                        size: needData.totalPeople.toString(),
-                        isRecurrent: true
-                    };
-                    console.log("Sending water request:", waterRequestData);
+                    const waterRequestData = { ...commonData, description: "water", categoryTreeId: needData.waterCategoryTreeId, quantity: needData.totalPeople, size: needData.totalPeople.toString(), isRecurrent: true };
+                    console.log("Creating water request:", waterRequestData);
                     await createNeed(localStorage.getItem('userId'), waterRequestData);
-
+                } else {
+                    console.log("Water request not sent. Data:", { water: needData.water });
                 }
 
-                // Handling food request
+
                 if (needData.food) {
-                    const foodRequestData = {
-                        ...commonData,
-                        description: "food",
-                        categoryTreeId: needData.foodCategoryTreeId,
-                        quantity: needData.totalPeople,
-                        size: needData.totalPeople.toString(),
-                        isRecurrent: true
-                    };
-                    console.log("Sending food request:", foodRequestData);
+                    const foodRequestData = { ...commonData, description: "food", categoryTreeId: needData.foodCategoryTreeId, quantity: needData.totalPeople, size: needData.totalPeople.toString(), isRecurrent: true };
+                    console.log("Creating food request:", foodRequestData);
                     await createNeed(localStorage.getItem('userId'), foodRequestData);
+                } else {
+                    console.log("Food request not sent. Data:", { food: needData.food });
                 }
 
-                // Handling clothing requests
-                for (const [category, quantity] of Object.entries(needData.clothingQuantities || {})) {
+
+                Object.entries(needData.clothingQuantities || {}).forEach(async ([category, quantity]) => {
                     if (parseInt(quantity, 10) > 0) {
-                        const clothingRequestData = {
-                            ...commonData,
-                            description: "clothing",
-                            categoryTreeId: 39, // Assuming 39 is the category ID for clothing
-                            quantity: parseInt(quantity, 10), // Convert string to integer
-                            size: category,
-                            isRecurrent: false
-                        };
-                        console.log(`Sending clothing request for ${category}:`, clothingRequestData);
+                        const clothingRequestData = { ...commonData, description: "clothing", categoryTreeId: 39, quantity: parseInt(quantity, 10), size: category, isRecurrent: false };
+                        console.log(`Creating clothing request for ${category}:`, clothingRequestData);
                         await createNeed(localStorage.getItem('userId'), clothingRequestData);
                     }
-                }
+                });
 
-                // Handling Shelter request
-                if (needData.shelterChecked) {
-                    const shelterRequestData = {
-                        ...commonData,
-                        description: "shelter",
-                        categoryTreeId: needData.shelterCategoryTreeId,
-                        quantity: 1,
-                        size: "M",
-                        isRecurrent: false
-                    };
-                    console.log("Sending shelter request:", shelterRequestData);
+                if (needData.shelter) {
+                    const shelterRequestData = { ...commonData, description: "shelter", categoryTreeId: 44, quantity: 1, size: "M", isRecurrent: false };
+                    console.log("Creating shelter request:", shelterRequestData);
                     await createNeed(localStorage.getItem('userId'), shelterRequestData);
+                } else {
+                    console.log("Shelter request not sent. Shelter:", { shelter: needData.shelter });
                 }
 
-                // Handling Heater request
-                if (needData.heaterChecked) {
-                    const heaterRequestData = {
-                        ...commonData,
-                        description: "heater",
-                        categoryTreeId: needData.heaterCategoryTreeId,
-                        quantity: 1,
-                        size: "M",
-                        isRecurrent: false
-                    };
-                    console.log("Sending heater request:", heaterRequestData);
+
+                if (needData.heater) {
+                    const heaterRequestData = { ...commonData, description: "heater", categoryTreeId: 46, quantity: 1, size: "M", isRecurrent: false };
+                    console.log("Creating heater request:", heaterRequestData);
                     await createNeed(localStorage.getItem('userId'), heaterRequestData);
+                } else {
+                    console.log("Heater request not sent. Data:", { heater: needData.heater });
                 }
 
-                // Handling Medicine requests
-                if (needData.medicineChecked) {
-                    needData.selectedIllnesses.forEach(async (illness) => {
-                        const quantity = needData.illnessCounts[illness] || 0;
-                        if (quantity > 0) {
-                            const medicineRequestData = {
-                                ...commonData,
-                                description: "medicine",
-                                categoryTreeId: needData.medicineCategoryTreeId,
-                                quantity: parseInt(quantity, 10),
-                                size: illness,
-                                isRecurrent: true
-                            };
-                            console.log(`Sending medicine request for ${illness}:`, medicineRequestData);
-                            await createNeed(localStorage.getItem('userId'), medicineRequestData);
-                        }
-                    });
-                }
 
-                console.log("All createNeed requests have been sent");
+                needData.selectedIllnesses?.forEach(async (illness) => {
+                    const quantity = needData.illnessCounts[illness] || 0;
+                    if (quantity > 0) {
+                        const medicineRequestData = { ...commonData, description: "medicine", categoryTreeId: 79, quantity: parseInt(quantity, 10), size: illness, isRecurrent: true };
+                        console.log(`Creating medicine request for ${illness}:`, medicineRequestData);
+                        await createNeed(localStorage.getItem('userId'), medicineRequestData);
+                    } else {
+                        console.log("Medicine request not sent. Medicine:", { medicine: needData.medicine });
+                    }
+
+                });
+
+                console.log("All requests have been created");
                 alert('Requests created successfully!');
             } catch (error) {
                 console.error('Error while creating requests:', error);
@@ -180,70 +111,36 @@ export default function Request() {
         }
     };
 
-
-
-
-    const handleBack = () => {
-        setActiveStep(activeStep - 1);
-    };
+    const handleBack = () => setActiveStep(activeStep - 1);
 
     return (
-        <MyNeedProvider>
-            <React.Fragment>
-                <ThemeProvider theme={customTheme}>
-                    <CssBaseline />
-                    <AppBar
-                        position="absolute"
-                        color="default"
-                        elevation={0}
-                        sx={{
-                            position: 'relative',
-                            borderBottom: (t) => `1px solid ${t.palette.divider}`,
-                        }}
-                    >
-                    </AppBar>
-                    <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
-                        <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-                            <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
-                                {steps.map((label) => (
-                                    <Step key={label}>
-                                        <StepLabel>{label}</StepLabel>
-                                    </Step>
-                                ))}
-                            </Stepper>
-                            {activeStep === steps.length ? (
-                                <React.Fragment>
-                                    <Typography variant="h5" gutterBottom>
-                                        Your request is saved.
-                                    </Typography>
-                                    <Typography variant="subtitle1">
-                                        Your request has been successfully created. We will process it promptly. You can track the status of your request on your profile page.                            </Typography>
-                                </React.Fragment>
-                            ) : (
-                                <React.Fragment>
-                                    {getStepContent(activeStep)}
-                                    <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-                                        {activeStep !== 0 && (
-                                            <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
-                                                Back
-                                            </Button>
-                                        )}
-
-                                        <Button
-                                            variant="contained"
-                                            onClick={handleNext}
-                                            sx={{ mt: 3, ml: 1 }}
-                                        >
-                                            {activeStep === steps.length - 1 ? 'Proceed' : 'Next'}
-                                        </Button>
-                                    </Box>
-                                </React.Fragment>
-                            )}
-                        </Paper>
-                        <Copyright />
-                    </Container>
-                </ThemeProvider>
-            </React.Fragment>
-        </MyNeedProvider>
+        <NeedProvider>
+            <ThemeProvider theme={customTheme}>
+                <CssBaseline />
+                <AppBar position="absolute" color="default" elevation={0} sx={{ position: 'relative', borderBottom: (t) => `1px solid ${t.palette.divider}` }} />
+                <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+                    <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
+                        <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
+                            {steps.map((label) => (<Step key={label}><StepLabel>{label}</StepLabel></Step>))}
+                        </Stepper>
+                        {activeStep === steps.length ? (
+                            <React.Fragment>
+                                <Typography variant="h5" gutterBottom>Your request is saved.</Typography>
+                                <Typography variant="subtitle1">Your request has been successfully created. We will process it promptly. You can track the status of your request on your profile page.</Typography>
+                            </React.Fragment>
+                        ) : (
+                            <React.Fragment>
+                                {getStepContent(activeStep)}
+                                <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                                    {activeStep !== 0 && (<Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>Back</Button>)}
+                                    <Button variant="contained" onClick={handleNext} sx={{ mt: 3, ml: 1 }}>{activeStep === steps.length - 1 ? 'Proceed' : 'Next'}</Button>
+                                </Box>
+                            </React.Fragment>
+                        )}
+                    </Paper>
+                    <Copyright />
+                </Container>
+            </ThemeProvider>
+        </NeedProvider>
     );
 }
