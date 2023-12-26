3123091b6a93e0811bfd25eb1b98335860b26cd9
Ilgaz Er <er.ilgaz@gmail.com>
2023-12-26 06:13:18 +0300

    implement selection of juman and material resources from category tree

--- resq/frontend/src/components/Resource/ResourceDetail1.js
+++ resq/frontend/src/components/Resource/ResourceDetail1.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 import {
     Typography,
     Grid,
@@ -8,15 +8,15 @@ import {
     Autocomplete,
     TextField
 } from '@mui/material';
-import { useQuery } from "@tanstack/react-query";
-import { getCategoryTree } from "../../AppService";
+import {useQuery} from "@tanstack/react-query";
+import {getCategoryTree} from "../../AppService";
 
-export default function ResourceDetails1({ resourceData, setResourceData }) {
+export default function ResourceDetails1({resourceData, setResourceData}) {
     const [isMaterialResourceChecked, setIsMaterialResourceChecked] = useState(false);
     const [isHumanResourceChecked, setIsHumanResourceChecked] = useState(false);
     const [selectedResource, setSelectedResource] = useState(null);
 
-    const { data: categoryTreeData, isLoading } = useQuery({
+    const {data: categoryTreeData, isLoading} = useQuery({
         queryKey: ['categoryTree'],
         queryFn: getCategoryTree
     });
@@ -26,14 +26,19 @@ export default function ResourceDetails1({ resourceData, setResourceData }) {
             resourceType: isMaterialResourceChecked ? 'material' : 'human',
             resourceId: selectedResource?.id || ''
         };
-        setResourceData({ ...resourceData, ...description });
+        setResourceData({...resourceData, ...description});
     }, [isMaterialResourceChecked, isHumanResourceChecked, selectedResource, setResourceData, resourceData]);
 
-    const filterCategoryItems = (type) => {
-        return categoryTreeData?.filter(cat => cat.type === type)
-            .map(cat => ({ label: cat.data, id: cat.id }))
-            .sort((a, b) => a.label.localeCompare(b.label)) || [];
-    };
+    const hrNode = categoryTreeData?.findCategoryWithId(12)
+    const humanResources = hrNode?.getLeafCategories() || []
+    const materialResources = categoryTreeData?.getLeafCategories()?.filter(node => !hrNode.isChildCategory(node.id)) || []
+
+    const comboOpts = (isMaterialResourceChecked ? materialResources : humanResources)
+        .map(cat => ({label: cat.data, id: cat.id}))
+        .sort((a, b) => {
+            if (a.label === b.label) return 0;
+            return a.label > b.label ? 1 : -1;
+        });
 
     return (
         <React.Fragment>
@@ -44,37 +49,38 @@ export default function ResourceDetails1({ resourceData, setResourceData }) {
                 <Grid item xs={12}>
                     <FormControlLabel
                         control={<Checkbox color="primary" checked={isMaterialResourceChecked}
-                            onChange={(e) => {
-                                setIsMaterialResourceChecked(e.target.checked);
-                                setIsHumanResourceChecked(!e.target.checked);
-                                setSelectedResource(null);
-                            }} />}
+                                           onChange={(e) => {
+                                               setIsMaterialResourceChecked(e.target.checked);
+                                               setIsHumanResourceChecked(!e.target.checked);
+                                               setSelectedResource(null);
+                                           }}/>}
                         label="Material Resource"
                     />
                     <FormControlLabel
                         control={<Checkbox color="primary" checked={isHumanResourceChecked}
-                            onChange={(e) => {
-                                setIsHumanResourceChecked(e.target.checked);
-                                setIsMaterialResourceChecked(!e.target.checked);
-                                setSelectedResource(null);
-                            }} />}
+                                           onChange={(e) => {
+                                               setIsHumanResourceChecked(e.target.checked);
+                                               setIsMaterialResourceChecked(!e.target.checked);
+                                               setSelectedResource(null);
+                                           }}/>}
                         label="Human Resource"
                     />
                 </Grid>
                 <Grid item xs={12}>
                     {(isMaterialResourceChecked || isHumanResourceChecked) && (
-                        <FormControl sx={{ m: 1, width: 300, mt: 3 }}>
+                        <FormControl sx={{m: 1, width: 300, mt: 3}}>
                             <Autocomplete
                                 disablePortal
                                 id="resource-category-combo-box"
-                                options={filterCategoryItems(isMaterialResourceChecked ? 'material' : 'human')}
+                                options={comboOpts}
                                 value={selectedResource}
                                 onChange={(event, newValue) => {
                                     setSelectedResource(newValue);
                                 }}
-                                sx={{ width: 300 }}
+                                sx={{width: 300}}
                                 renderInput={(params) => (
-                                    <TextField {...params} label={isMaterialResourceChecked ? "Material Resource" : "Human Resource"} />
+                                    <TextField {...params}
+                                               label={isMaterialResourceChecked ? "Material Resource" : "Human Resource"}/>
                                 )}
                             />
                         </FormControl>
